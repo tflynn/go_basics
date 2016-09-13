3b367db01b77b20867773720aa44739a8cbcc82e
Tracy Flynn <tracysflynn@gmail.com>
2016-09-13 07:58:13 -0400

    Added all random types to endpoint

--- urand/server/randomCache.go
+++ urand/server/randomCache.go
@@ -13,8 +13,26 @@ var (
 	randomSetCache = cache.New(-1, 60*time.Second)
 )
 type RandomEntry struct {
-	index uint64 // Index entry
-	values []uint64 // Array of values
+	indexUint64 uint64 // uint64 index entry
+	valuesUint64 []uint64 // Array of uint64 values
+	indexUint32 uint32 // uint32 index entry
+	valuesUint32 []uint32 // Array of uint32 values
+	indexUint16 uint16 // uint16 index entry
+	valuesUint16 []uint16 // Array of uint16 values
+	indexUint8 uint8 // uint8 index entry
+	valuesUint8 []uint8 // Array of uint8 values
+	indexUint uint // uint index entry
+	valuesUint []uint // Array of uint values
+	indexInt64 int64 // int64 index entry
+	valuesInt64 []int64 // Array of int64 values
+	indexInt32 int32 // int32 index entry
+	valuesInt32 []int32 // Array of int32 values
+	indexInt16 int16 // int16 index entry
+	valuesInt16 []int16 // Array of int16 values
+	indexInt8 int8 // int8 index entry
+	valuesInt8 []int8 // Array of int8 values
+	indexInt int // int index entry
+	valuesInt []int // Array of int values
 }
 
 type RandomSet struct {
@@ -22,11 +40,11 @@ type RandomSet struct {
 }
 
 type unexportedInterface interface {
-	Set(string, string, *RandomSet, time.Duration)
+	Set(*RandomSet, time.Duration)
 	Get(string, string) (*RandomSet, bool)
 }
 
-func secToDuration(sec int64) time.Duration {
+func secToDuration(sec uint64) time.Duration {
 	//fmt.Println("secToDuration sec ", sec)
 	//fmt.Println("secToDuration time.Duration(sec) * time.Second ", time.Duration(sec) * time.Second)
 	return time.Duration(sec) * time.Second
@@ -36,24 +54,24 @@ func durationToSec(d time.Duration) int64 {
 	return int64(d.Nanoseconds() / time.Second.Nanoseconds())
 }
 
-func entryName(expiration int64) string {
-	return fmt.Sprintf("%d.seconds",expiration)
+func entryName(expiration uint64, randomType string) string {
+	return fmt.Sprintf("%s,%d.seconds",randomType, expiration)
 }
 
 // Add an item to the cache, replacing any existing item.
 // The key is assumed to be some numbers of seconds i.e. 3 * time.Second
-func Set(randomSet *RandomSet, durationInSecs int64) {
-	randomSetCache.Set(entryName(durationInSecs),randomSet,secToDuration(durationInSecs))
+func Set(randomSet *RandomSet, durationInSecs uint64, randomType string) {
+	randomSetCache.Set(entryName(durationInSecs, randomType),randomSet,secToDuration(durationInSecs))
 }
 
 // Get an item from the cache. Returns the item or nil, and a bool indicating
 // The key is assumed to be some numbers of seconds i.e. 3 * time.Second
 // The entry will be created if not present
-func Get(durationInSecs int64) (*RandomSet, bool) {
-	eName := entryName(durationInSecs)
+func Get(durationInSecs uint64, randomType string, setSize uint64 ) (*RandomSet, bool) {
+	eName := entryName(durationInSecs, randomType)
 	_, present := randomSetCache.Get(eName)
 	if ! present {
-		randomSet := generateRandomSet(durationInSecs)
+		randomSet := generateRandomSet(durationInSecs, randomType, setSize)
 		randomSetCache.Set(eName,randomSet,secToDuration(durationInSecs))
 	}
 	entry, present := randomSetCache.Get(eName)
@@ -67,35 +85,68 @@ func Get(durationInSecs int64) (*RandomSet, bool) {
 }
 
 
-func generateRandomEntry(durationInSecs int64) *RandomEntry {
-	totalValues := uint64(calcTotalValues(durationInSecs) + 1) // Extra is index
+func generateRandomEntry(randomType string, setSize uint64) *RandomEntry {
 	randomEntry := new(RandomEntry)
-	randomValues := core.GetUInt64(totalValues,false)
-	randomEntry.index = randomValues[0]
-	randomEntry.values = randomValues[1:]
+	switch randomType {
+	case "uint64":
+		setSize := setSize + 1 // Extra is index
+		randomValuesUint64 := core.GetUInt64(setSize,false)
+		randomEntry.indexUint64 = randomValuesUint64[0]
+		randomEntry.valuesUint64 = randomValuesUint64[1:]
+	case "uint32":
+		setSize := setSize + 1 // Extra is index
+		randomValuesUint32 := core.GetUInt32(setSize,false)
+		randomEntry.indexUint32 = randomValuesUint32[0]
+		randomEntry.valuesUint32 = randomValuesUint32[1:]
+	case "uint16":
+		setSize := setSize + 1 // Extra is index
+		randomValuesUint16 := core.GetUInt16(setSize,false)
+		randomEntry.indexUint16 = randomValuesUint16[0]
+		randomEntry.valuesUint16 = randomValuesUint16[1:]
+	case "uint8":
+		setSize := setSize + 1 // Extra is index
+		randomValuesUint8 := core.GetUInt8(setSize,false)
+		randomEntry.indexUint8 = randomValuesUint8[0]
+		randomEntry.valuesUint8 = randomValuesUint8[1:]
+	case "int64":
+		setSize := setSize + 1 // Extra is index
+		randomValuesInt64 := core.GetInt64(setSize,false)
+		randomEntry.indexInt64 = randomValuesInt64[0]
+		randomEntry.valuesInt64 = randomValuesInt64[1:]
+	case "int32":
+		setSize := setSize + 1 // Extra is index
+		randomValuesInt32 := core.GetInt32(setSize,false)
+		randomEntry.indexInt32 = randomValuesInt32[0]
+		randomEntry.valuesInt32 = randomValuesInt32[1:]
+	case "int16":
+		setSize := setSize + 1 // Extra is index
+		randomValuesInt16 := core.GetInt16(setSize,false)
+		randomEntry.indexInt16 = randomValuesInt16[0]
+		randomEntry.valuesInt16 = randomValuesInt16[1:]
+	case "int8":
+		setSize := setSize + 1 // Extra is index
+		randomValuesInt8 := core.GetInt8(setSize,false)
+		randomEntry.indexInt8 = randomValuesInt8[0]
+		randomEntry.valuesInt8 = randomValuesInt8[1:]
+	default:
+		fmt.Println("generateRandomEntry unknown random type found %s",randomType)
+	}
 	return randomEntry
 }
 
-func generateRandomSet(durationInSecs int64) *RandomSet {
+func generateRandomSet(durationInSecs uint64, randomType string, setSize uint64) *RandomSet {
 	totalEntries := calcTotalEntries(durationInSecs)
 	randomEntries := make([]*RandomEntry,totalEntries, totalEntries)
-	for i := int64(0) ; i < totalEntries ; i++ {
-		randomEntries[i] = generateRandomEntry(durationInSecs)
+	for i := uint64(0) ; i < totalEntries ; i++ {
+		randomEntries[i] = generateRandomEntry(randomType, setSize)
 	}
 	randomSet := new(RandomSet)
 	randomSet.entries = randomEntries
 	return randomSet
 }
 
-// Determine how many values in an entry
-func calcTotalValues(secs int64) int64 {
-	//TODO Algorithm needed
-	//return int64(1000) // 1000 random numbers per entry
-	return int64(10) // 1000 random numbers per entry //DEBUG
-}
-
 // Determine how many entries in a set
-func calcTotalEntries(secs int64) int64 {
+func calcTotalEntries(secs uint64) uint64 {
 	//TODO Algorithm needed
-	return int64(1 * secs) // 1 set per  second
+	return uint64(1 * secs) // 1 set per  second
 }
--- urand/server/server.go
+++ urand/server/server.go
@@ -5,8 +5,17 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 	//	"log"
 //	"time"
+
+)
+
+const (
+	minsetSize int = 1
+	maxsetSize int = 1000
+	minRetentionSecs int = 1
+	maxRetentionSecs int = 60
 )
 
 func RunServer(router *gin.Engine, port string) {
@@ -16,7 +25,7 @@ func RunServer(router *gin.Engine, port string) {
 
 func AddRoutes(router *gin.Engine) {
 	router.GET("/health", healthHandler)
-	router.GET("/random/:randomType", randomTypeHandler)
+	router.GET("/random/set/:randomType", randomSetHandler)
 }
 
 func healthHandler(c *gin.Context) {
@@ -29,16 +38,58 @@ func healthHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, msg)
 }
 
-func randomTypeHandler(c *gin.Context) {
-	randomSet,_ := Get(5)
-	for i := 0 ; i < len(randomSet.entries); i++ {
-		entry := randomSet.entries[i]
-		fmt.Println("entry.index ", entry.index)
-		fmt.Println("entry.values ", entry.values)
-	}
-
+func randomSetHandler(c *gin.Context) {
 	randomType := c.Param("randomType")
-	total := c.DefaultQuery("total", "10")
-	c.String(http.StatusOK, "%s total %s", randomType, total)
+	setSizeParam := c.DefaultQuery("setSize", "10")
+	setSize, err1 := strconv.Atoi(setSizeParam)
+	retentionParam := c.DefaultQuery("retention", "1")
+	retention, err2 := strconv.Atoi(retentionParam)
+	fmt.Printf("randomSetHandler: %s retention %s setSize %s\n",randomType, retentionParam, setSizeParam)
+	if err1 != nil || err2 != nil {
+		if err1 != nil {
+			c.String(http.StatusBadRequest, "Invalid setSize %s", randomType, setSizeParam)
+		}
+		if err2 != nil {
+			c.String(http.StatusBadRequest, "Invalid retention period %s", randomType, retentionParam)
+		}
+	} else {
+		validationError := false
+		if setSize < minsetSize || setSize > maxsetSize {
+			c.String(http.StatusBadRequest, "Invalid setSize %s", randomType, setSizeParam)
+			validationError = true
+		}
+		if retention < minRetentionSecs || retention > maxRetentionSecs {
+			c.String(http.StatusBadRequest, "Invalid retention period %s", randomType, retentionParam)
+			validationError = true
+		}
+		if ! validationError {
+			randomSet,_ := Get(uint64(retention),randomType, uint64(setSize))
+			for i := 0 ; i < len(randomSet.entries); i++ {
+				entry := randomSet.entries[i]
+				switch randomType {
+				case "uint64":
+					c.JSON(http.StatusOK, gin.H{"index": entry.indexUint64, "values": entry.valuesUint64})
+				case "uint32":
+					c.JSON(http.StatusOK, gin.H{"index": entry.indexUint32, "values": entry.valuesUint32})
+				case "uint16":
+					c.JSON(http.StatusOK, gin.H{"index": entry.indexUint16, "values": entry.valuesUint16})
+				case "uint8":
+					c.JSON(http.StatusOK, gin.H{"index": entry.indexUint8, "values": entry.valuesUint8})
+				case "int64":
+					c.JSON(http.StatusOK, gin.H{"index": entry.indexInt64, "values": entry.valuesInt64})
+				case "int32":
+					c.JSON(http.StatusOK, gin.H{"index": entry.indexInt32, "values": entry.valuesInt32})
+				case "int16":
+					c.JSON(http.StatusOK, gin.H{"index": entry.indexInt16, "values": entry.valuesInt16})
+				case "int8":
+					c.JSON(http.StatusOK, gin.H{"index": entry.indexInt8, "values": entry.valuesInt8})
+				default:
+					c.JSON(http.StatusBadRequest,  gin.H{"Error": fmt.Sprintf("Unknown random type %s",randomType)})
+				}
+			}
+
+			//c.String(http.StatusOK, "%s setSize %d", randomType, setSize)
+		}
+	}
 }
 
